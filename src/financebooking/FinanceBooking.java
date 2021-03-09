9c018c65fdc222713220335497c571a1fb8d1935
Maksym Godovanets <maksymgodovanets@gmail.com>
2021-03-09 19:59:27 +0100

    Add 2 new file -> Transaction.java, Transfer.java. Mofifield -> FinanceBooking.class ,Account.java

--- src/financebooking/FinanceBooking.java
+++ src/financebooking/FinanceBooking.java
@@ -10,19 +10,20 @@ import java.util.logging.Logger;
 
 public class FinanceBooking {
 
-    public static void main(String[] args){
-	init ();
-	//System.out.println (Text.get("PROGRAM_NAME"));
-	//System.out.println ( Arrays.toString(Text.getMonths ()));
+    public static void main(String[] args) {
+        init ();
+        //System.out.println (Text.get("PROGRAM_NAME"));
+        //System.out.println ( Arrays.toString(Text.getMonths ()));
     }
+
     private static void init() {
-        try{
+        try {
             Text.init ();
             GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment ();
-            ge.registerFont ( Font.createFont ( Font.TRUETYPE_FONT, new File ("fonts/FontsNaturalMonoAlt.ttf") ) );
+            ge.registerFont ( Font.createFont ( Font.TRUETYPE_FONT, new File ( "fonts/FontsNaturalMonoAlt.ttf" ) ) );
 
-        }catch (FontFormatException | IOException exc){
-            Logger.getLogger ( FinanceBooking.class.getName ()).log( Level.SEVERE,null,exc );
+        } catch (FontFormatException | IOException exc) {
+            Logger.getLogger ( FinanceBooking.class.getName () ).log ( Level.SEVERE, null, exc );
         }
 
     }
